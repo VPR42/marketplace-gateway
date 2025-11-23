81375698e027e2a8bb7f6a9a0d8548207a7a255d
Запара Иван <truthpruthbg@gmail.com>
2025-11-23 16:34:34 +0300

    INIT: Шаблон подогнан под итоговый проект

--- src/main/kotlin/com/vpr42/marketplacegateway/MarketplaceGatewayApplication.kt
+++ src/main/kotlin/com/vpr42/marketplacegateway/MarketplaceGatewayApplication.kt
@@ -1,11 +1,11 @@
-package com.vpr42.kotlintemplaterepository
+package com.vpr42.marketplacegateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinTemplateRepositoryApplication
+class MarketplaceGatewayApplication
 
 fun main(args: Array<String>) {
-    runApplication<KotlinTemplateRepositoryApplication>(*args)
+    runApplication<MarketplaceGatewayApplication>(*args)
 }
