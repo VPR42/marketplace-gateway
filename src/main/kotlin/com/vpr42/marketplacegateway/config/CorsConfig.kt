1a154aed29e521694d1bdb5568eddbedab321290
Запара Иван <truthpruthbg@gmail.com>
2025-11-26 02:08:04 +0300

    HOTFIX: Правка корсов (я уже плачу очень сильно аж умереть хочеца)

--- src/main/kotlin/com/vpr42/marketplacegateway/config/CorsConfig.kt
+++ src/main/kotlin/com/vpr42/marketplacegateway/config/CorsConfig.kt
@@ -1,19 +1,32 @@
 package com.vpr42.marketplacegateway.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.config.CorsRegistry
-import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @Configuration
-class CorsConfig : WebFluxConfigurer {
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOrigins(
+class CorsConfig {
+    @Bean
+    fun corsWebFilter(): CorsWebFilter {
+        val config = CorsConfiguration().apply {
+            allowCredentials = true
+
+            // Явно указываем фронты
+            allowedOrigins = listOf(
                 "http://localhost:5173",
                 "https://hack.kinoko.su"
             )
-            .allowedMethods("*")
-            .allowedHeaders("*")
-            .allowCredentials(true)
+
+            addAllowedHeader("*")
+            addAllowedMethod("*")
+        }
+
+        val source = UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", config)
+        }
+
+        return CorsWebFilter(source)
     }
 }
