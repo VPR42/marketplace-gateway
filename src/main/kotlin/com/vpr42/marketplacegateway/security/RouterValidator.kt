ac51187ce1453fd7363dc86c03377dc805002833
Запара Иван <truthpruthbg@gmail.com>
2025-11-27 11:45:22 +0300

    VJ-62: Добавлена открытость сваггер документации, логи проверки подключения к юрике на error уровне

--- src/main/kotlin/com/vpr42/marketplacegateway/security/RouterValidator.kt
+++ src/main/kotlin/com/vpr42/marketplacegateway/security/RouterValidator.kt
@@ -3,19 +3,37 @@ package com.vpr42.marketplacegateway.security
 import com.vpr42.marketplacegateway.properties.ApplicationProperties
 import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.stereotype.Component
+import org.springframework.util.AntPathMatcher
 import java.util.function.Predicate
 
 @Component
 class RouterValidator(
     private val applicationProperties: ApplicationProperties
 ) {
-    var isSecured: Predicate<ServerHttpRequest> = Predicate<ServerHttpRequest> { request ->
-        applicationProperties
-            .routes
-            .filter { it.isSecure }
-            .stream()
-            .noneMatch {
-                request.uri.path.contains(it.uri)
-            }
+    private val pathMatcher = AntPathMatcher()
+
+    private val swaggerUrls = listOf(
+        "/api/**/docs/**",
+        "/api/**/swagger",
+        "/api/**/swagger-ui/**",
+    )
+
+    val isSecured: Predicate<ServerHttpRequest> = Predicate<ServerHttpRequest> { request ->
+        val path = request.uri.path
+
+        if (isSwaggerPath(path)) {
+            false
+        } else {
+            applicationProperties
+                .routes
+                .filter { it.isSecure }
+                .stream()
+                .noneMatch {
+                    path.contains(it.uri)
+                }
+        }
     }
+
+    private fun isSwaggerPath(path: String): Boolean =
+        swaggerUrls.any { pattern -> pathMatcher.match(pattern, path) }
 }
